383d54e9eef392d60efee6d95ae38a0995a1729a
chandu42242682 <97518840+chandu42242682@users.noreply.github.com>
2022-09-14 12:47:29 +0530

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -2,6 +2,8 @@ terraform {
 
   required_providers {
     aws = {
+      acess_key = AKIATU5ZYTMOU3KJ7KN5
+      secret_key = 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
       source  = "hashicorp/aws"
       version = "4.11.0"
     }
